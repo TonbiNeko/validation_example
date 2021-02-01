6363f356a442c32d6329673ffaaa8f07140bf452
Junko Mikami <junkomikami312nz@gmail.com>
2021-02-01 14:01:57 +0900

    Create event function

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_01_045040) do
+ActiveRecord::Schema.define(version: 2021_02_01_050121) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.datetime "start_datetime"
+    t.datetime "end_datetime"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
