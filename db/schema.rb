5d5fa2cb5e2ba07bc7eb507d3013a3c8696f3cae
Junko Mikami <junkomikami312nz@gmail.com>
2021-02-01 13:52:02 +0900

    Create movie function

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_01_040224) do
+ActiveRecord::Schema.define(version: 2021_02_01_045040) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "production_year"
+    t.integer "running_time"
+    t.string "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
