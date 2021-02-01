ca0d3bf3c98e9c54c3df0b15647bf3ace6a17bee
Junko Mikami <junkomikami312nz@gmail.com>
2021-02-01 14:13:11 +0900

    Add validations to event

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,5 @@
 class Blog < ApplicationRecord
-  validates :title, presence: true
-  validates :title, presence: {message: "が空になっています。何か入力してください。"}
+  # validates :title, presence: true
+  # validates :title, presence: {message: "が空になっています。何か入力してください。"}
   validates :title, presence: true, on: :update
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,11 @@
 class Event < ApplicationRecord
+  # validate :start_datetime_cannot_be_later_than_end_datetime
+  # private
+  # def start_datetime_cannot_be_later_than_end_datetime
+  #   if start_datetime > end_datetime
+  #     errors.add(:start_datetime, "終了日時より後に設定出来ません")
+  #   end
+  # end
+  include ActiveModel::Validations
+  validates_with EventValidator
 end
